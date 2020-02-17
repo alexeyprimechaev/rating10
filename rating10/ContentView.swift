c5cd71cc89f34909d8db8a2e9fd1fab221a8655c
алёша <asprimechaev@icloud.com>
2020-02-17 14:30:35 +0300

    Initial Commit

---
+++ rating10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rating10
+//
+//  Created by Alexey Primechaev on 2/17/20.
+//  Copyright © 2020 Alexey Primechaev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
